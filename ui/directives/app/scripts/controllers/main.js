565c35b1f486e8240e527f013d64e0a3b84df6ce
Mike Thompson <mithomps@redhat.com>
2014-05-05 16:13:54 -0700

    Initial Yeoman Project Template.

---
+++ ui/directives/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('chartingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
