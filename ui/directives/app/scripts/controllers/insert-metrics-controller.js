66c9cc2ea5b3d03188d348b68e108562b4b336c0
Mike Thompson <mithomps@redhat.com>
2014-05-15 12:04:19 -0700

    Added transfer-encoding: chunked

--- ui/directives/app/scripts/controllers/insert-metrics-controller.js
+++ ui/directives/app/scripts/controllers/insert-metrics-controller.js
@@ -17,7 +17,14 @@ angular.module('chartingApp')
         $scope.insertData = function () {
 
             console.log("POSTing data");
-            $http.post('/rhq-metrics/' + $scope.insertData.id + '/data', $scope.insertData.jsonPayload
+            $http({
+                    url: 'http://localhost:7474/rhq-metrics/' + $scope.insertData.id + '/data',
+                    method: 'POST',
+                    data: $scope.insertData.jsonPayload,
+                    headers: {'Transfer-Encoding': 'chunked',
+                        'Access-Control-Allow-Headers': 'Content-Type, x-xsrf-token',
+                        'Content-Type': 'application/json'}
+                }
             ).success(function (response, status) {
 
                     console.debug("POST response: " + status + " --> " + response);
